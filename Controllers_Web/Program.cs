c05d2a50bf829cf735735e2b5033fe5609e41926
peacetran2018 <peacetran2018@gmail.com>
2024-08-22 07:51:38 +0800

    Controller and IActionResult - File Results - New lession FileContentResult - There are 3 ways to return file result

--- Controllers_Web/Program.cs
+++ Controllers_Web/Program.cs
@@ -3,7 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();//Add Controllers as service in the IServiceCollection.
 
 var app = builder.Build();
-
+app.UseStaticFiles();//enable static files - default folder name wwwroot
+app.UseRouting();
 app.MapControllers();//Add alls action method as endpoints.
 
 app.Run();
