2831b8495e44166d45158e5864ca0e95da17d3a2
Peace Tran <peacetran2018@gmail.com>
2024-08-22 23:09:55 +0800

    Develop (#5)
    * Update Readme
    
    * Controllers and IActionResult - IActionResult (#4)
    
    - Add IActionResult sample project
    - Add define of IActionResult to Readme

---
+++ IActionResult_Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IActionResult_Web.Controllers;
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index(){
+        //IActionResult can be used for all return action methods in same method
+        if(!Request.Query.ContainsKey("bookid")){
+            Response.StatusCode = 400;
+            return Content("Book ID is not supplied");
+        }
+
+        if(string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"]))){
+            Response.StatusCode = 400;
+            return Content("Book ID cannot be blank");
+        }
+
+        int bookid = Convert.ToInt32(Request.Query["bookid"]);
+        if(bookid < 0){
+            Response.StatusCode = 400;
+            return Content("Book ID cannot be less than 0");
+        }
+        if(bookid > 1000){
+            Response.StatusCode = 400;
+            return Content("Book ID cannot be greater than 1000");
+        }
+        Response.StatusCode = 200;
+        return File("/sample.pdf", "application/pdf");
+    }
+}
